47c3e131c95e5e368ad150ed428fe9ba07a25d30
muj <99077742+muj-i@users.noreply.github.com>
2023-06-01 18:05:25 +0600

    ,

--- practice_app/lib/main.dart
+++ practice_app/lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+void main() => runApp(NewApp()
+    // MaterialApp(
+    //   home: Center(
+    //     child: Text('Hello world'),
+    //   ),
+    // ),
+    );
+
+class NewApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Fu*king World'),
+          centerTitle: true,
+          actions: [
+            IconButton(icon: Icon(Icons.add_alarm), onPressed: () {})
+          ],
+        ),
       ),
-    ),
-  );
+    );
+    // TODO: implement build
+    //throw UnimplementedError();
+  }
 }
