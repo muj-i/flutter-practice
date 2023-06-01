6862bfbf79ee1e4005d3208d3ed70f92b1e847af
muj <99077742+muj-i@users.noreply.github.com>
2023-06-01 19:38:58 +0600

    Update main.dart

--- prac_app/lib/main.dart
+++ prac_app/lib/main.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(NewApp()
-  // MaterialApp(
-  //   home: Center(
-  //     child: Text('Hello world'),
-  //   ),
-  // ),
-);
-
-class NewApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Fu*king World'),
-          centerTitle: true,
-          actions: [
-            IconButton(icon: Icon(Icons.add_alarm), onPressed: () {})
-          ],
-        ),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello, world!'),
       ),
-    );
-    // TODO: implement build
-    //throw UnimplementedError();
-  }
+    ),
+  );
 }
+
+
+// class NewApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Hello Fu*king World'),
+//           centerTitle: false,
+//           //leading: Icon(Icons.ac_unit), //onPressed: () {}
+//           actions: [
+//             IconButton(icon: Icon(Icons.add_alarm), onPressed: () {}),
+//             IconButton(icon: Icon(Icons.account_circle), onPressed: () {})
+//           ],
+//         ),
+//         body: Center(
+//           child: Text(
+//             'pop the puilder',
+//             style: TextStyle(fontSize: 38, fontStyle: FontStyle.italic),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
