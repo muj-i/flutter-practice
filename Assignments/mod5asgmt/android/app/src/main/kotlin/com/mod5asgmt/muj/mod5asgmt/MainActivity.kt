884f27a3ed163088d30db1bfe8ad51b0b3d212b0
muj <99077742+muj-i@users.noreply.github.com>
2023-06-04 00:38:19 +0600

    initial

---
+++ Assignments/mod5asgmt/android/app/src/main/kotlin/com/mod5asgmt/muj/mod5asgmt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mod5asgmt.muj.mod5asgmt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- prac_app/lib/main.dart
+++ prac_app/lib/main.dart
@@ -1,38 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello, world!'),
-      ),
-    ),
-  );
+  runApp();
 }
-
-
-// class NewApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Hello Fu*king World'),
-//           centerTitle: false,
-//           //leading: Icon(Icons.ac_unit), //onPressed: () {}
-//           actions: [
-//             IconButton(icon: Icon(Icons.add_alarm), onPressed: () {}),
-//             IconButton(icon: Icon(Icons.account_circle), onPressed: () {})
-//           ],
-//         ),
-//         body: Center(
-//           child: Text(
-//             'pop the puilder',
-//             style: TextStyle(fontSize: 38, fontStyle: FontStyle.italic),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
