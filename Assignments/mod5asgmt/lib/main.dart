dbd732537ec998c9b9cbf74d3278145247d5ac90
muj <99077742+muj-i@users.noreply.github.com>
2023-06-04 13:29:55 +0600

    Update assignment for module 5

--- Assignments/mod5asgmt/lib/main.dart
+++ Assignments/mod5asgmt/lib/main.dart
@@ -1,20 +1,25 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'pages.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeActivity(),
-      debugShowCheckedModeBanner: false,);
+    return MaterialApp(
+      home: HomeActivity(),
+    
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
 class HomeActivity extends StatefulWidget {
+
   @override
   _HomeActivityState createState() {
     return _HomeActivityState();
@@ -22,49 +27,62 @@ class HomeActivity extends StatefulWidget {
 }
 
 class _HomeActivityState extends State<HomeActivity> {
-  int _currentIndex = 1;
+  
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Module 5 Assignment"),
-        backgroundColor: const Color.fromARGB(255, 130, 31, 89).withAlpha(2000),
+        backgroundColor: const Color.fromARGB(255, 10, 29, 66).withAlpha(2000),
       ),
       drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(child: Text("Module 5 Assignment")),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Page 1"),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 0;
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.business_center_outlined),
-              title: Text("Page 2"),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 1;
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.people),
-              title: Text("Page 3"),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 2;
-                  Navigator.pop(context);
-                });
-              },
-            ),
-          ],
+        child: Container(
+          color: const Color.fromARGB(255, 10, 29, 66).withAlpha(200),
+          child: ListView(
+            children: [
+              DrawerHeader(child: Text("Module 5 Assignment", 
+              style: TextStyle(fontSize: 44, color: Colors.white70),
+              )),
+              ListTile(
+                iconColor: Colors.white,
+                textColor: Colors.white,
+                leading: Icon(Icons.home),
+                title: Text("Page 1"),
+                onTap: () {
+                  setState(() {
+                    _currentIndex = 0;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              ListTile(
+                iconColor: Colors.white,
+                textColor: Colors.white,
+                leading: Icon(Icons.business_center_outlined),
+                title: Text("Page 2"),
+                onTap: () {
+                  setState(() {
+                    _currentIndex = 1;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              ListTile(
+                iconColor: Colors.white,
+                textColor: Colors.white,
+                leading: Icon(Icons.people),
+                title: Text("Page 3"),
+                onTap: () {
+                  setState(() {
+                    _currentIndex = 2;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
       body: IndexedStack(
@@ -75,86 +93,31 @@ class _HomeActivityState extends State<HomeActivity> {
           Page3(),
         ],
       ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-        child: GNav(
-          iconSize: 24,
-          padding: EdgeInsets.all(16),
-          activeColor: const Color.fromARGB(255, 130, 31, 89),
-          tabBackgroundColor:
-              const Color.fromARGB(255, 130, 31, 89).withOpacity(0.1),
-          gap: 8,
-          tabs: [
-            GButton(icon: Icons.home, text: "Page 1"),
-            GButton(icon: Icons.business_center_outlined, text: "Page 2"),
-            GButton(icon: Icons.people, text: "Page 3"),
-          ],
-          onTabChange: (int index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
+      bottomNavigationBar: Container(
+        color: const Color.fromARGB(255, 10, 29, 66).withAlpha(200),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: GNav(
+            backgroundColor: const Color.fromARGB(255, 10, 29, 66).withAlpha(0),
+            color: Colors.white,
+            iconSize: 24,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 9),
+            activeColor: Colors.white,
+            tabBackgroundColor:
+                Color.fromARGB(255, 228, 231, 238).withOpacity(0.1),
+            gap: 8,
+            tabs: [
+              GButton(icon: Icons.home, text: "Page 1"),
+              GButton(icon: Icons.business_center_outlined, text: "Page 2"),
+              GButton(icon: Icons.people, text: "Page 3"),
+            ],
+            onTabChange: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
         ),
-      ),
-    );
-  }
-}
-
-class Page1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Page 1",
-          style: TextStyle(fontSize: 32),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 10,
-        child: Icon(Icons.abc_rounded),
-        onPressed: () {},
-        backgroundColor: Color.fromARGB(255, 130, 31, 89),
-      ),
-    );
-  }
-}
-
-class Page2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Page 2",
-          style: TextStyle(fontSize: 32),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 10,
-        child: Icon(Icons.document_scanner_outlined),
-        onPressed: () {},
-        backgroundColor: Colors.blueAccent,
-      ),
-    );
-  }
-}
-
-class Page3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Page 3",
-          style: TextStyle(fontSize: 32),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 10,
-        child: Icon(Icons.add),
-        onPressed: () {},
-        backgroundColor: Colors.red,
       ),
     );
   }
---
+++ Assignments/mod5asgmt/lib/pages.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Center(
+        child: Text(
+          "Page 1",
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.abc_rounded),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                "Page 1 Floating Action Button",
+              ),
+            ),
+          );
+        },
+        backgroundColor: Color.fromARGB(255, 10, 29, 66),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      body: Center(
+        child: Text(
+          "Page 2",
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.document_scanner_outlined),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                "Page 2 Floating Action Button",
+              ),
+            ),
+          );
+        },
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[100],
+      body: Center(
+        child: Text(
+          "Page 3",
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.add),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                "Page 3 Floating Action Button",
+              ),
+            ),
+          );
+        },
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
