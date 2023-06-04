a7191400b9b938a9b2e6b13f8c6d4493529c7527
muj <99077742+muj-i@users.noreply.github.com>
2023-06-04 15:52:48 +0600

    Update beta2.dart

--- Assignments/mod5asgmt/temp/beta2.dart
+++ Assignments/mod5asgmt/temp/beta2.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'pages.dart';
+import 'package:mod5asgmt/pages.dart';
 
 void main() {
   runApp(MyApp());
