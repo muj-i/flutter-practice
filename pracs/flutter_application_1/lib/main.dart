92df4413fec14108d3f6895a9e177416ee5b9c4d
muj <99077742+muj-i@users.noreply.github.com>
2023-06-07 00:32:00 +0600

    ....................

--- pracs/flutter_application_1/lib/main.dart
+++ pracs/flutter_application_1/lib/main.dart
@@ -24,27 +24,113 @@ class HomeAct extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("klkok"),
+        title: Text("kelkok"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.screen_search_desktop_sharp),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings_applications_sharp),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.security_sharp),
+          )
+        ],
         backgroundColor: Colors.red,
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                  ),
+                  accountName: Text('muj'),
+                  accountEmail: Text('mio'),
+                  currentAccountPicture: Image.asset('images/td.png'),
+                )),
+            ListTile(
+              leading: Icon(Icons.person_2_sharp),
+              title: Text('Profile'),
+            )
+          ],
+        ),
       ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(),
-        Text(
-          "New App!",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-        ),
-        Text(
-          "New App2!",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-        ),
-        Text(
-          "New App!",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-        ),
-      ]),
+            Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+              children: [Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 9, 17, 237),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.symmetric(horizontal: 37.5000, vertical: 20),
+                  child: Text(
+                    "New !",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 9, 176, 242),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "New App!",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 233, 162, 30),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "New App!",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                
+              ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 233, 30, 196),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "New App!",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 30, 233, 50),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "New App!",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+            ),
+          ]),
     );
   }
 }
--- pracs/prac_app/android/app/src/main/kotlin/com/prac_app/muj/prac_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.prac_app.muj.prac_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
