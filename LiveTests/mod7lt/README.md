cbf454cc9a78cd50f62e214a2f0d4a2c23a276aa
MUJ <99077742+muj-i@users.noreply.github.com>
2023-06-19 23:36:10 +0600

    module 7 live test

---
+++ LiveTests/mod7lt/README.md
@@ -0,0 +1,17 @@
+# mod7Livetest
+
+A flutter project for the live test of module 7
+
+## Screenshots
+Other screenshots are individually wrapped below for faster loading of the repository.
+
+## Full app view
+![Alt Text](images/screenshots/ss1.png)
+
+![*Dialog box*](images/screenshots/ss2.png)
+
+![*Second screen*](images/screenshots/ss3.png)
+
+![*Decrement functionality*](images/screenshots/ss4.png)
+
+
---
+++ LiveTests/mod7lt/lib/second_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 20,
+        title: Text('Congratulations'),
+      ),
+      body: Center(
+        child: Text(
+          'Congratulations! You reached 10!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
