1b57e2766091be1015ab4c08a9ef4ca9767644fe
MUJ <99077742+muj-i@users.noreply.github.com>
2023-06-24 23:40:56 +0600

    added assignment module 8

---
+++ Assignments/mod8asgmt/android/app/src/main/kotlin/com/example/mod8asgmt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mod8asgmt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Assignments/mod8asgmt/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:theme_provider/theme_provider.dart';
+import 'home.dart';
+
+// TODO 1 - Button for switch theme
+// TODO 2 - Update todo
+// TODO 3 - Update the ui design of bottom sheet
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ThemeProvider(
+      themes: [
+        AppTheme(
+          data: ThemeData.light().copyWith(
+            useMaterial3: true,
+            colorScheme:
+                ColorScheme.fromSeed(seedColor: Colors.indigo.shade900),
+          ),
+          id: 'custom_theme_light',
+          description: 'Dark Theme',
+        ),
+        AppTheme(
+          id: "custom_theme_dark",
+          description: "Dark Theme",
+          data: ThemeData.dark(
+            useMaterial3: true,
+          ),
+        ),
+      ],
+      child: ThemeConsumer(
+        child: Builder(
+          builder: (themeContext) => MaterialApp(
+            theme: ThemeProvider.themeOf(themeContext).data,
+            title: 'Material App',
+            home: HomeScreen(),
+            debugShowCheckedModeBanner: false,
+          ),
+        ),
+      ),
+    );
+  }
+}
