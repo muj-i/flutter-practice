bf17d8236a0cd83644ee28ee7e1f4a1193c506c6
MUJ <99077742+muj-i@users.noreply.github.com>
2023-06-25 18:09:51 +0600

    r&d date time picker

--- Assignments/mod8asgmt/lib/home.dart
+++ Assignments/mod8asgmt/lib/home.dart
@@ -1,175 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class HomeScreen extends StatefulWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Date Time Picker Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DateTimePickerDemo(),
+    );
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<Task> tasks = [];
+class DateTimePickerDemo extends StatefulWidget {
+  @override
+  _DateTimePickerDemoState createState() => _DateTimePickerDemoState();
+}
+
+class _DateTimePickerDemoState extends State<DateTimePickerDemo> {
+  DateTime? selectedDate;
+  TimeOfDay? selectedTime;
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2010),
+      lastDate: DateTime(2030),
+    );
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+
+    if (picked != null && picked != selectedTime) {
+      setState(() {
+        selectedTime = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task Management List'),
+        title: Text('Date Time Picker Demo'),
       ),
-      body: ListView.separated(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(tasks[index].title),
-            subtitle: Text(tasks[index].description),
-            onTap: () => _openTaskDetails(tasks[index]),
-            onLongPress: () => _showDeleteTaskBottomSheet(tasks[index]),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const Divider(
-            height: 0,
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showAddTaskDialog(),
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _showAddTaskDialog() {
-    String title = '';
-    String description = '';
-    String deadline = '';
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Add Task'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                onChanged: (value) => title = value,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Description'),
-                onChanged: (value) => description = value,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Deadline'),
-                onChanged: (value) => deadline = value,
-              ),
-            ],
-          ),
-          actions: [
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
             ElevatedButton(
-              onPressed: () {
-                if (title.isNotEmpty &&
-                    description.isNotEmpty &&
-                    deadline.isNotEmpty) {
-                  final newTask = Task(
-                    title: title,
-                    description: description,
-                    deadline: deadline,
-                  );
-                  setState(() {
-                    tasks.add(newTask);
-                  });
-                  Navigator.of(context).pop();
-                }
-              },
-              child: Text('Save'),
+              onPressed: () => _selectDate(context),
+              child: Text('Select Date'),
+            ),
+            ElevatedButton(
+              onPressed: () => _selectTime(context),
+              child: Text('Select Time'),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Selected Date: ${selectedDate != null ? selectedDate.toString() : 'Not selected'}',
+            ),
+            Text(
+              'Selected Time: ${selectedTime != null ? '${selectedTime?.hour}:${selectedTime?.minute}' : 'Not selected'}',
             ),
           ],
-        );
-      },
+        ),
+      ),
     );
   }
-
-  void _openTaskDetails(Task task) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Task Details',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Text('Title: ${task.title}'),
-              Text('Description: ${task.description}'),
-              Text('Deadline: ${task.deadline.toString()}'),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    tasks.remove(task);
-                  });
-                  Navigator.of(context).pop();
-                },
-                child: Text('Delete'),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  void _showDeleteTaskBottomSheet(Task task) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Delete Task',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Text('Are you sure you want to delete this task?'),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    tasks.remove(task);
-                  });
-                  Navigator.of(context).pop();
-                },
-                child: Text('Delete'),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-class Task {
-  final String title;
-  final String description;
-  final String deadline;
-
-  Task({
-    required this.title,
-    required this.description,
-    required this.deadline,
-  });
 }
