bbe2752ae155588777bac1d42cc608bbd1e02fd7
MUJ <99077742+muj-i@users.noreply.github.com>
2023-08-23 03:04:59 +0600

    build removed

--- Assignments/mod8asgmt/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/HDD-Z/dxProgramming/Flutter-Practice/Assignments/mod8asgmt/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Volumes/HDD-Z/dxProgramming/Flutter-Practice/Assignments/mod8asgmt/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- Assignments/mod8asgmt/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/Objects-normal/x86_64/shared_preferences_foundation_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/HDD-Z/dxProgramming/Flutter-Practice/Assignments/mod8asgmt/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/module.modulemap \
-  /Volumes/HDD-Z/dxProgramming/Flutter-Practice/Assignments/mod8asgmt/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/DerivedSources/shared_preferences_foundation_vers.c
